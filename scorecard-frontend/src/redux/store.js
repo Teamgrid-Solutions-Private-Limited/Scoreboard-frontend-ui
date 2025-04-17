3a8da78583a23d27aff2d7515a94d60af51f26ee
Sakil7430 <sakil.hossain@teamgrid.io>
2025-04-17 16:55:43 +0530

    added store

--- scorecard-frontend/src/redux/store.js
+++ scorecard-frontend/src/redux/store.js
@@ -1,16 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import senatorReducer from "./action-reducer/senatorSlice";
-import senatorDataSlice from "./action-reducer/senatorTermSlice"
-import houseReducer from "./action-reducer/houseSlice"
-import houseDataReducer from "./action-reducer/houseTermSlice"
-import voteReducer from "./action-reducer/voteSlice"
-const store=configureStore({
-    reducer:{
-        senator:senatorReducer,
-        senatorData:senatorDataSlice,
-        house:houseReducer,
-        houseData:houseDataReducer ,
-        vote:voteReducer
-    }
+import VoteReducer from "../redux/Reducer/voteSlice"
+import termReducer from "../redux/Reducer/termSlice"
+
+const store = configureStore({
+  reducer: {
+    vote : VoteReducer,
+   
+  }
 });
+
 export default store;
