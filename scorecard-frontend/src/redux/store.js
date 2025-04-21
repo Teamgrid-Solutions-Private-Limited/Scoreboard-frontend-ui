b916c85eee3398b82643661517ebd65f55b10b76
Sakil7430 <sakil.hossain@teamgrid.io>
2025-04-21 11:53:51 +0530

    fixing merge

--- scorecard-frontend/src/redux/store.js
+++ scorecard-frontend/src/redux/store.js
@@ -2,20 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import senatorReducer from "./action-reducer/senatorSlice";
 import senatorDataSlice from "./action-reducer/senatorTermSlice"
 import houseReducer from "./action-reducer/houseSlice"
-import houseDataReducer from "./action-reducer/houseTermSlice"
-import voteReducer from "./action-reducer/voteSlice"
-import activityReducer from "./action-reducer/activitySlice"
-import termSlice from "./action-reducer/termSlice";
-const store=configureStore({
-    reducer:{
-        senator:senatorReducer,
-        senatorData:senatorDataSlice,
-        house:houseReducer,
-        houseData:houseDataReducer ,
-        vote:voteReducer,
-        activity:activityReducer,
-        term:termSlice
-    }
+import termReducer from "../redux/Reducer/termSlice"
+
+const store = configureStore({
+  reducer: {
+    vote : VoteReducer,
+   
+  }
 });
 
 export default store;
